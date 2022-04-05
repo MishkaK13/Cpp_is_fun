88810585fdbc76ed87b61d07ba698574b4cc1467
Михаил <65108560+MishkaK13@users.noreply.github.com>
2022-04-05 21:05:01 +0300

    Update Source.cpp
    Задание 2.

--- how_good_can_you_multiply/Source.cpp
+++ how_good_can_you_multiply/Source.cpp
@@ -1,11 +1,25 @@
+#include <stdio.h>
 #include <iostream>
-
 using namespace std;
 
-int main() {
-
-	setlocale(0, "");
-
-	system("pause");
-	return 0;
+int main()
+{
+    int i, j, center = 5;
+    for (i = 0; i < center*2; i++)
+    {
+        for (j = 0; j < center * 2; j++)
+        {
+            if (i <= center)
+            {
+              (j >= center - i && j <= center + i)?cout << "*":cout << " ";
+            }
+            else
+            {
+              (j >= center + i - center * 2 + 1 && j <= center - i + center * 2  - 1)?cout << "*":cout << " ";
+            }
+        }
+        cout << "\n\n\n\n";
+    }
+    system("pause");
+    return 0;
 }
